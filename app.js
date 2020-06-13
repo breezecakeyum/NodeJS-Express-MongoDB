8b01bc61896e77b111983da6115861aa7625176a
Gene Harrel <generharrel@gmail.com>
2020-06-12 22:32:10 -0700

    Express Session Part 1

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
 const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/nucampsite';
@@ -34,8 +37,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth(req, res, next) {
-    if (!req.signedCookies.user) {
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
+
+function auth (req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
             const err = new Error('You are not authenticated!');
@@ -48,7 +61,7 @@ function auth(req, res, next) {
         const user = auth[0];
         const pass = auth[1];
         if (user === 'admin' && pass === 'password') {
-            res.cookie('user', 'admin', {signed: true});
+            req.session.user = 'admin';
             return next(); // authorized
         } else {
             const err = new Error('You are not authenticated!');
@@ -57,7 +70,8 @@ function auth(req, res, next) {
             return next(err);
         }
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'admin') {
+            console.log('req.session:', req.session);
             return next();
         } else {
             const err = new Error('You are not authenticated!');
@@ -66,6 +80,7 @@ function auth(req, res, next) {
         }
     }
 }
+
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
