2deba4b0f2f92b1c7727928739199b5c08b91b9c
Gene Harrel <generharrel@gmail.com>
2020-06-20 07:38:10 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '550977152239296',
+        clientSecret: '8b7ee5e368528b363250b3e16c37ed0e'
+    }
 }
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+
 const userSchema = new Schema({
     firstname: {
         type: String,
@@ -11,6 +12,7 @@ const userSchema = new Schema({
     type: String,
         default: ''
     },
+    facebookId: String,
     admin: {
         type: Boolean,
         default: false
